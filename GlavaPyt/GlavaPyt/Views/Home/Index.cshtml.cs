1a365730e3a195d8a198ea4e60fb4b61a5dae57d
Ivan Kourtev <135639014+IvanKourtev@users.noreply.github.com>
2024-07-13 15:55:28 +0300

    Changed Pages folder to Views

--- GlavaPyt/GlavaPyt/Views/Home/Index.cshtml.cs
+++ GlavaPyt/GlavaPyt/Views/Home/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace GlavaPyt.Pages
+namespace GlavaPyt.Views.Home
 {
     public class IndexModel : PageModel
     {
